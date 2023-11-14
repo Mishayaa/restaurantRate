078e79d3070115078a10a84aed23826beb340efc
Mishayaa <misha.chayka.2004@gmail.com>
2023-11-14 15:01:39 +0200

    new dtos

---
+++ src/main/java/com/example/restaurantestimate/dto/google/Location.java
@@ -0,0 +1,17 @@
+package com.example.restaurantestimate.dto.google;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Location {
+    private Double lat;
+    private Double lng;
+
+    // getters and setters
+}
---
+++ src/main/java/com/example/restaurantestimate/dto/google/OpeningHours.java
@@ -0,0 +1,15 @@
+package com.example.restaurantestimate.dto.google;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OpeningHours {
+    private Boolean open_now;
+
+}
