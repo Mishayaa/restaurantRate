bd9d1b6fc831106c25c97720c8104a3d914bb5a4
Mishayaa <misha.chayka.2004@gmail.com>
2023-11-25 17:24:55 +0200

    path update

--- src/main/java/com/example/restaurantestimate/constant/ConstUrls.java
+++ src/main/java/com/example/restaurantestimate/constant/ConstUrls.java
@@ -15,19 +15,15 @@ import static java.lang.invoke.VarHandle.AccessMode.GET;
 import static javax.swing.text.html.FormSubmitEvent.MethodType.POST;
 
 public class ConstUrls {
+
     public static final RequestMatcher PUBLIC_URLS = new OrRequestMatcher(
             new AntPathRequestMatcher(AUTH_CONTROLLER_REFRESH_TOKEN_PATH, GET.toString()),
-            new AntPathRequestMatcher(AUTH_CONTROLLER_REFRESH_TOKEN_PATH, GET.toString()),
             new AntPathRequestMatcher(AUTH_CONTROLLER_LOGIN_PATH, POST.toString()),
-            new AntPathRequestMatcher(AUTH_CONTROLLER_LOGIN_PATH, GET.toString()),
             new AntPathRequestMatcher(USER_CONTROLLER_PATH, POST.toString()),
-            new AntPathRequestMatcher("/**", GET.toString()),
-            new AntPathRequestMatcher("/**", POST.toString()),
-
             new AntPathRequestMatcher(RESTAURANT_CONTROLLER_PATH+"/restaurantsNames",GET.toString()),
             new AntPathRequestMatcher(RESTAURANT_CONTROLLER_PATH_USERS+"/**",GET.toString()),
             new AntPathRequestMatcher(RESTAURANT_CONTROLLER_PATH+"/popular",GET.toString()),
-            new AntPathRequestMatcher(RESTAURANT_CONTROLLER_PATH+"/favorites"),
+            new AntPathRequestMatcher(RESTAURANT_CONTROLLER_PATH+"/favorites",GET.toString()),
             new AntPathRequestMatcher(RESTAURANT_CONTROLLER_PATH+"/*"),
             new AntPathRequestMatcher("/api/auth/login/home"),
             new AntPathRequestMatcher("/h2/**"),
